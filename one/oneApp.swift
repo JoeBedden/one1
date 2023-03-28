12003d546e7c5546c9a2df02524edc596902102a
LeiZiKang <lzkhaoshua@icloud.com>
2023-03-29 01:53:28 +0800

    Initial Commit

---
+++ one/oneApp.swift
@@ -0,0 +1,17 @@
+//
+//  oneApp.swift
+//  one
+//
+//  Created by 雷子康 on 2023/3/29.
+//
+
+import SwiftUI
+
+@main
+struct oneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
